7a73c962c00056c18264c114c80891a5a5f99c0b
jvenator <j@venator.vc>
2015-04-27 15:00:48 -0400

    Brocfile fixes

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,11 @@
-var compileSass = require('broccoli-sass');
 var pickFiles = require('broccoli-static-compiler');
+var compileSass = require('broccoli-sass');
 var mergeTrees = require('broccoli-merge-trees');
 
 var styles = pickFiles('styles', { srcDir: '/', destDir: 'assets' });
 
-var sourceTrees = [styles]
+var sourceTrees = [styles];
 
-var siteCss = compileSass(styles, 'assets/global.scss', 'assets/global.css');
+var siteCss = compileSass(sourceTrees, 'assets/global.scss', 'assets/global.css');
 
 module.exports = siteCss;
